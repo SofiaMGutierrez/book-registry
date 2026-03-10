053e8372594fc986855ddc5da5701bd1b64c748b
Sofia Gutierrez <sofiagutierrez@Sofias-MacBook-Air.local>
2026-03-10 16:18:10 -0500

    Initial commit - Library Book Registry API

---
+++ src/main/java/com/library/book_registry/BookRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.library.book_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRegistryApplication.class, args);
+	}
+
+}
